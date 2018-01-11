e7315c84274c2291a3ebfafe51344170e305ec98
Sandro <sandrolima@gmail.com>
2018-01-11 03:17:50 -0200

    page admin etc..

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
-use  Apsys\Page;
+//use  Apsys\Page;
+use  Apsys\PageAdmin;
 
 $app = new Slim();
 
@@ -14,6 +15,16 @@ $app->config('debug', true);
 
 $app->get('/', function() {
 	$page = new Page();
+	$page->setTpl("index");
+
+	//$sql = new Apsys\DB\Sql();
+  //$res = $sql->select("SELECT * FROM tb_users");
+  //echo json_encode($res);
+
+});
+
+$app->get('/admin', function() {
+	$page = new PageAdmin();
 	$page->setTpl("index");
 
 	//$sql = new Apsys\DB\Sql();
